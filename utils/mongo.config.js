a71b3b020fd45e30f85cc4cdef439b07a3406d57
BigB97 <bigbofficial01@gmail.com>
2022-01-12 00:08:12 +0100

    update

--- utils/mongo.config.js
+++ utils/mongo.config.js
@@ -7,7 +7,7 @@ const options = {
 
 module.exports = async () => {
      try {
-          await mongoose.connect("mongodb+srv://Bolaji:12345678@Test@cluster0.ywv3e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", options)
+          await mongoose.connect("mongodb+srv://Bolaji:12345678%40Test@cluster0.ywv3e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", options)
           console.log(':::> Connected to MongoDB database')
      } catch (error) {
           console.log("<::: Couldn't connect to database ", error)
